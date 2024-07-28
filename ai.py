6e4533c430e362ad92d116f125c149fecd161280
Raiyan Hasan <raiyanhasan2006@gmail.com>
2024-07-28 15:06:01 +0530

    ai code added

--- ai.py
+++ ai.py
@@ -1,2 +1,38 @@
-def get_ai_response(str):
-    return f"You print this '{str}'"
+
+
+import os
+
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyAuG3U8LyzDBhbupNO7ppm89bYMS0LYxp4")
+
+
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  generation_config=generation_config,
+  # safety_settings = Adjust safety settings
+  # See https://ai.google.dev/gemini-api/docs/safety-settings
+  system_instruction="you are jarvis a large langugae model",
+  tools='code_execution',
+)
+
+chat_session = model.start_chat(
+  history=[
+  ]
+)
+def get_ai_response(query):
+    response = chat_session.send_message(query)
+    return response.text
+
+
+
+if __name__ == "__main__":
+    print(get_ai_response("How are you"))
--- alpha.py
+++ alpha.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
-import ai as ai
-app= Flask(__name__)
+import ai as ai  # Import the ai module
+
+app = Flask(__name__)
 
 @app.route("/")
 def alpha_home():
     return render_template('./index.html')
 
-@app.route("/response")
-def aires():
-    return ai.get_ai_response("Hi")
+@app.route("/response/<query>")
+def ai_res(query):
+    response = ai.get_ai_response(query)  # Call the get_ai_response function
+    return response  # Return the AI's response
 
 if __name__ == "__main__":
     app.run()
